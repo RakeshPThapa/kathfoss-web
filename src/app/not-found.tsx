bc02e748620cee244780564516c0daae352a8c48
nischalshakya <107358076+nischalshakya787@users.noreply.github.com>
2025-01-24 23:56:54 +0545

    Fix/navbar (#65)
    * commented out TImer
    
    * added nabar in layout
    
    * fixed a navbar bug
    
    * fixed the navbar bug

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,19 +6,21 @@ const NotFound = () => {
   return (
     <Box
       sx={{
-        position: "relative",
-        zIndex: 1,
         textAlign: "center",
-        marginTop: "20%",
+        padding: 4,
       }}
     >
-      <Typography variant="h1">404</Typography>
-      <Typography variant="h5">Page Not Found</Typography>
-      <Link href="/" passHref>
-        <Button variant="contained" color="primary" sx={{ marginTop: "20px" }}>
-          Go to Home
-        </Button>
-      </Link>
+      <Box sx={{ marginTop: "15rem" }}>
+        <Typography variant="h1">404</Typography>
+        <Typography variant="h5" gutterBottom>
+          Page Not Found
+        </Typography>
+        <Link href="/" passHref>
+          <Button variant="contained" color="primary">
+            Go to Home
+          </Button>
+        </Link>
+      </Box>
     </Box>
   );
 };
